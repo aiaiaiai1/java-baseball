6655ddbf533652215bd5ad3a780fe8710eee76f3
GyeongHwanBaek <cvc100326@naver.com>
2022-12-07 19:10:48 +0900

    refactor: 랜덤한여러숫자 생성 기능 수정
    - 반환타입 Number 에서 List<Integer> 로 변경
    - 중복되지 않는 여러 숫자 생성으로 수정

--- src/main/java/baseball/domain/RandomNumbersGenerator.java
+++ src/main/java/baseball/domain/RandomNumbersGenerator.java
@@ -1,19 +1,22 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class RandomNumbersGenerator {
-    private static final int NUMBERS_LENGTH = 3;
-    public Numbers generate() {
-        List<Number> numbers = new ArrayList<>();
-        while (numbers.size() < NUMBERS_LENGTH) {
-            numbers.add(new Number());
+    private static final int RANDOM_LOWER_INCLUSIVE = 1;
+    private static final int RANDOM_UPPER_INCLUSIVE = 9;
+
+    public List<Integer> generateNotDuplicateNumbers(int length) {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < length) {
+            int number = Randoms.pickNumberInRange(RANDOM_LOWER_INCLUSIVE, RANDOM_UPPER_INCLUSIVE);
+            if (!numbers.contains(number)) {
+                numbers.add(number);
+            }
         }
-        try {
-            return new Numbers(numbers);
-        } catch (IllegalArgumentException e) {
-            return generate();
-        }
+        return numbers;
     }
 }
