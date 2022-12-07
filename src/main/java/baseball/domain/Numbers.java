ead969ac7b591af32c11db66b242f5c1d0ee3989
GyeongHwanBaek <cvc100326@naver.com>
2022-12-07 15:33:20 +0900

    feat: 숫자가 중복인경우와 3자리가 아닌경우 예외 기능 추가

--- src/main/java/baseball/domain/Numbers.java
+++ src/main/java/baseball/domain/Numbers.java
@@ -1,2 +1,34 @@
-package baseball.domain;public class Numbers {
+package baseball.domain;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Numbers {
+    private static final int NUMBERS_LENGTH = 3;
+    private final List<Number> numbers;
+
+    public Numbers(List<Number> numbers){
+        this.numbers = numbers;
+    }
+
+    private void validateLength(List<Number> numbers){
+        if(numbers.size() != NUMBERS_LENGTH){
+            throw new IllegalArgumentException();
+        }
+    }
+    private void validateDuplication(List<Number> numbers){
+        List<Integer> tmp = new ArrayList<>();
+        for(Number numnber : numbers){
+            tmp.add(numnber.getNumber());
+        }
+        Set<Integer> tmp2 = new HashSet<>(tmp);
+        if(tmp.size() != tmp2.size()){
+            throw new IllegalArgumentException();
+        }
+    }
+
+
+
 }
