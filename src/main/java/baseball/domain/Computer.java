38fec7df2f1db0b1e8907fdcd312e6fd6e350692
GyeongHwanBaek <cvc100326@naver.com>
2022-12-07 23:29:05 +0900

    feat: 숫자 비교해서 힌트 확인하는 기능 추가

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -6,10 +6,22 @@ import java.util.List;
 public class Computer {
     private Numbers numbers;
 
-    public Computer() {
-
+    public Computer(List<Integer> numbers) {
+        this.numbers = new Numbers(numbers);
     }
 
+    public List<Integer> checkHint(List<Integer> numbers){
+        List<Integer> result = new ArrayList<>();
+        int ball = this.numbers.contain(numbers);
+        int strike = this.numbers.match(numbers);
+        result.add(ball-strike);
+        result.add(strike);
+        return result;
+    }
 
 }
+
+
+
+
 
