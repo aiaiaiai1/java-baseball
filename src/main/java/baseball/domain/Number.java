bcd6a20f8b3280cd3dccdc4a53034882b7f8b1f5
GyeongHwanBaek <cvc100326@naver.com>
2022-12-07 12:44:03 +0900

    feat: 1~9사이의 숫자 만들기 기능 추가

--- src/main/java/baseball/domain/Number.java
+++ src/main/java/baseball/domain/Number.java
@@ -1,2 +1,25 @@
-package baseball.domain;public class Number {
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Number {
+    private static final int NUMBER_LOWER_INCLUSIVE = 1;
+    private static final int NUMBER_UPPER_INCLUSIVE = 9;
+
+    private final int number;
+
+    public Number(int number) {
+        validate(number);
+        this.number = number;
+    }
+
+    public Number(){
+        this.number = Randoms.pickNumberInRange(NUMBER_LOWER_INCLUSIVE, NUMBER_UPPER_INCLUSIVE);
+    }
+
+    private void validate(int number){
+        if(number > NUMBER_UPPER_INCLUSIVE || number < NUMBER_LOWER_INCLUSIVE){
+            throw new IllegalArgumentException();
+        }
+    }
 }
