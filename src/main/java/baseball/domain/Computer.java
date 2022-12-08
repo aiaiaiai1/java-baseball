7566c7d6a8f58d5d47dfab903e54196537b1f7b5
GyeongHwanBaek <cvc100326@naver.com>
2022-12-08 20:34:33 +0900

    refactor: 힌트 얻는 기능 분리 , 파라미터있는 생성자 삭제
    - 볼 갯수 , 스트라이크 갯수 구하는 기능으로 따로 분리
    - 필드 랜덤숫자 생성객체 추가, 숫자초기화기능 추가

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -5,20 +5,31 @@ import java.util.List;
 
 public class Computer {
     private Numbers numbers;
+    private final RandomNumbersGenerator randomNumbersGenerator = new RandomNumbersGenerator();
 
-    public Computer(List<Integer> numbers) {
-        this.numbers = new Numbers(numbers);
+    public void initiateNumbers(){
+        this.numbers = new Numbers(generate());
     }
 
-    public List<Integer> checkHint(List<Integer> numbers){
+    protected List<Integer> generate(){
+       return randomNumbersGenerator.generateNotDuplicateNumbers(3);
+    }
+
+/*    public List<Integer> checkHint(List<Integer> numbers){
         List<Integer> result = new ArrayList<>();
         int ball = this.numbers.contain(numbers);
         int strike = this.numbers.match(numbers);
         result.add(ball-strike);
         result.add(strike);
         return result;
+    }*/
+    public int countBall(List<Integer> numbers){
+        return this.numbers.contain(numbers);
     }
 
+    public int countStrike(List<Integer> numbers){
+        return this.numbers.match(numbers);
+    }
 }
 
 
