4d00b858153cada3b329ef63a78fd27f0a3f0146
GyeongHwanBaek <cvc100326@naver.com>
2022-12-08 11:53:26 +0900

    feat: 입력기능 추가
    - 숫자 입력기능
    - 명령어 입력기능
    - 입력값이 숫자가 아니면 예외처리

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,4 +1,25 @@
-package baseball;
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private static final String INPUT_ONLY_NUMBERS = "[ERROR] 숫자를 입력해주세요";
+
+    public int readNumbers() {
+        try {
+            String input = Console.readLine();
+            return Integer.parseInt(input);
+        } catch (IllegalArgumentException notNumbers) {
+            throw new IllegalArgumentException(INPUT_ONLY_NUMBERS);
+        }
+    }
+
+    public int readCommand() {
+        try {
+            String input = Console.readLine();
+            return Integer.parseInt(input);
+        } catch (IllegalArgumentException notNumbers) {
+            throw new IllegalArgumentException(INPUT_ONLY_NUMBERS);
+        }
+    }
 }
