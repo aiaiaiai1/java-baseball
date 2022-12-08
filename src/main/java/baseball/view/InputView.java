4fc961ef6711f0aecfd8c1f553013c21bfef4f5c
GyeongHwanBaek <cvc100326@naver.com>
2022-12-08 16:46:27 +0900

    refactor: 메서드 반환타입 변경
    - int 에서 List<Integer> 로 변경

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -2,13 +2,19 @@ package baseball.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputView {
     private static final String INPUT_ONLY_NUMBERS = "[ERROR] 숫자를 입력해주세요";
 
-    public int readNumbers() {
+    public List<Integer> readNumbers() {
+        List<Integer> numbers = new ArrayList<>();
         try {
-            String input = Console.readLine();
-            return Integer.parseInt(input);
+            for (char character : Console.readLine().toCharArray()) {
+                numbers.add(Integer.parseInt(String.valueOf(character)));
+            }
+            return numbers;
         } catch (IllegalArgumentException notNumbers) {
             throw new IllegalArgumentException(INPUT_ONLY_NUMBERS);
         }
