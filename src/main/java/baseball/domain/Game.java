8653d3ea6b885e57f36544db45d54dd8ef8c598a
GyeongHwanBaek <cvc100326@naver.com>
2022-12-09 12:54:01 +0900

    feat: Game 클래스 추가

---
+++ src/main/java/baseball/domain/Game.java
@@ -0,0 +1,21 @@
+package baseball.domain;
+
+
+import java.util.List;
+
+public class Game {
+    private Numbers computerNumbers;
+    private final RandomNumbersGenerator randomNumbersGenerator = new RandomNumbersGenerator();
+    public void initiateComputer(){
+        computerNumbers = new Numbers(generateRandomNumbers());
+    }
+    protected List<Integer> generateRandomNumbers(){
+        return randomNumbersGenerator.generateNotDuplicateNumbers(3);
+    }
+
+    public Result getResult(Numbers userNumbers){
+        int ballCount = computerNumbers.contain(userNumbers.getNumbers());
+        int strikeCount = computerNumbers.match(userNumbers.getNumbers());
+        return new Result(ballCount,strikeCount);
+    }
+}
